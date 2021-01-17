92e3295ce6058ac2338fd3f7bc46fc3d864255f9
Pavel Miron <pavelmir@ext.zara.com>
2021-01-17 17:01:41 +0100

    APIService is generated

---
+++ app/src/main/java/md/pavel/icehoney/pagedspaceflightarticles/list/data/APIService.kt
@@ -0,0 +1,28 @@
+package md.pavel.icehoney.pagedspaceflightarticles.list.data
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import md.pavel.icehoney.pagedspaceflightarticles.list.ApiArticle
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+interface APIService {
+
+    @GET("/positions.json")
+    suspend fun getArticles(): Response<List<ApiArticle>>
+
+    companion object {
+
+        private val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        fun getApiService() = Retrofit.Builder()
+            .baseUrl("https://test.spaceflightnewsapi.net")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(APIService::class.java)
+    }
+}
